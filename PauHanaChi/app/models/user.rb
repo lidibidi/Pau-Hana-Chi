8572ce9a0e308c44b5845e9167205549bbf803cf
Lidia Santos <lidiams28@gmail.com>
2016-01-21 20:38:43 -0600

    added geocoder

--- PauHanaChi/app/models/user.rb
+++ PauHanaChi/app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-  
+
+  #simple coding by ip_address > http://www.rubygeocoder.com/ 
+  geocoded_by :ip_address, :latitude => :latitude, :longitude => :longitude
+  after_validation :geocode
+
 end
--- PauHanaChi/db/schema.rb
+++ PauHanaChi/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122021534) do
+ActiveRecord::Schema.define(version: 20160122022046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20160122021534) do
     t.datetime "updated_at",      null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "ip_address"
   end
 
 end
