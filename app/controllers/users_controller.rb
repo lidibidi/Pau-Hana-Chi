965ae753683ca909871ec85bf232ce6caadd8156
Lidia Santos <lidiams28@gmail.com>
2016-01-25 20:37:18 -0600

    working users crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_action :logged_in_user, only: [:index, :edit, :update]
+before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
 before_action :correct_user,   only: [:edit, :update]
+before_action :admin_user,     only: :destroy
 # before_action :authenticate, :authorize, :only [:edit, :update, :show]
   def show
     @user = User.find(params[:id])
@@ -41,6 +42,12 @@ def update
    end
 end
 
+def destroy
+  User.find(params[:id]).destroy
+  flash[:success] = "User deleted"
+  redirect_to users_url
+end
+
 def authenticate
   unless logged_in?
     redirect_to root_url
@@ -75,4 +82,9 @@ private
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
+  # Confirms an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
